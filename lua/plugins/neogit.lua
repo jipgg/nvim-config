99ca5789918e778a946450c457d1da0608557f5a
Jip <83951137+jipgg@users.noreply.github.com>
2024-09-14 05:16:26 +0200

    remove unnecessary neogit dependencies

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,8 +6,6 @@ return {
 
     -- Only one of these is needed.
     "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua",              -- optional
-    "echasnovski/mini.pick",         -- optional
   },
   config = true
 }
